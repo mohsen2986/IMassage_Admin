376832144bd6107b8f9bdfe7c56a301c996b4466
mohsen <mohsenaliakbari.atom@hotmail.com>
2020-11-21 14:52:51 +0330

    final Impls

--- app/src/main/java/com/example/imassage_admin/ui/activity/MainActivity.kt
+++ app/src/main/java/com/example/imassage_admin/ui/activity/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.imassage_admin
+package com.example.imassage_admin.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.imassage_admin.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
