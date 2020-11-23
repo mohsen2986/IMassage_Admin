c57ceb8335713ad255c250294183b60d5e5cfc8d
mohsen <mohsenaliakbari.atom@hotmail.com>
2020-11-23 10:42:16 +0330

    update ApiInterface.kt

--- app/src/main/java/com/example/imassage_admin/data/remote/api/ApiInterface.kt
+++ app/src/main/java/com/example/imassage_admin/data/remote/api/ApiInterface.kt
@@ -1,7 +1,9 @@
 package com.example.imassage_admin.data.remote.api
 
 import com.example.imassage_admin.data.model.AboutUs
-import com.example.imassage_admin.data.remote.model.ErrorResponse
+import com.example.imassage_admin.data.model.Boarder
+import com.example.imassage_admin.data.model.Question
+import com.example.imassage_admin.data.remote.model.*
 import com.haroldadmin.cnradapter.NetworkResponse
 import okhttp3.MultipartBody
 import okhttp3.RequestBody
@@ -24,9 +26,6 @@ interface ApiInterface{
 
     @Multipart
     @POST("aboutUs/{id}")
-//    @Headers(
-//        "Content-Type: application/x-www-form-urlencoded"
-//    )
     suspend fun updateAboutUs(
         @Part file: MultipartBody.Part ,
         @Path("id") id: String ,
@@ -34,5 +33,85 @@ interface ApiInterface{
         @Query("description") description_: String = "this is a test"
     ): NetworkResponse< AboutUs , ErrorResponse>
 
+    // sliders
+    @GET("boarder")
+    suspend fun sliders(
+    ): NetworkResponse< BoarderResponse , ErrorResponse>
+
+    @Multipart
+    @POST("boarder")
+    suspend fun uploadSlider(
+            @Part file: MultipartBody.Part ,
+            @Query("title") title: String ,
+            @Query("description") description: String
+    ): NetworkResponse< Boarder , ErrorResponse>
+
+    @DELETE("boarder/{id}")
+    suspend fun deleteSlider(
+            @Path("id") id: String
+    ): NetworkResponse<Boarder , ErrorResponse>
+
+    // massage
+    @GET("massage")
+    suspend fun massages(
+    ): NetworkResponse<MassageResponse, ErrorResponse>
+
+    @Multipart
+    @POST("massage")
+    suspend fun uploadMassage(
+            @Part image: MultipartBody.Part ,
+            @Query("name") name: String ,
+            @Query("cost") cost: String ,
+            @Query("length") length: String ,
+            @Query("description") description: String
+    ):NetworkResponse<MassageResponse ,ErrorResponse>
+
+    @DELETE("massage/{id}")
+    suspend fun deleteMassage(
+            @Path("id") id: String
+    ): NetworkResponse<MassageResponse , ErrorResponse>
+
+    // packages
+    @GET("packages")
+    suspend fun packages(
+    ): NetworkResponse<PackageResponse, ErrorResponse>
+
+    @Multipart
+    @POST("packages")
+    suspend fun uploadPackage(
+        @Part image: MultipartBody.Part ,
+        @Query("name") name: String ,
+        @Query("description") description: String ,
+        @Query("cost") cost: String ,
+        @Query("massage_id") massageId: String
+    ): NetworkResponse<PackageResponse ,ErrorResponse>
+
+    @DELETE("packages/{id}")
+    suspend fun deletePackage(
+        @Path("id") id: String
+    ): NetworkResponse<PackageResponse , ErrorResponse>
+
+    // questions
+    @GET("questions")
+    suspend fun questions(
+    ): NetworkResponse<QuestionResponse, ErrorResponse>
+
+    @POST("question")
+    suspend fun uploadQuestion(
+            @Query("question") question: String ,
+            @Query("question_type_id") questionTypeId: String = "1" ,
+            @Query("form_id") formId: String = "1"
+    ): NetworkResponse< Question , ErrorResponse>
+
+    @DELETE("question/{id}")
+    suspend fun deleteQuestion(
+            @Path("id") id: String
+    ): NetworkResponse<Question , ErrorResponse>
+
+    // Users
+    @GET("users")
+    suspend fun users(
+            @Query("page") page: Int?
+    ): NetworkResponse< UserResponse , ErrorResponse>
 
 }
