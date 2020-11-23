2aee14b093edacc481f136a604299abc74aed8b4
mohsen <mohsenaliakbari.atom@hotmail.com>
2020-11-23 10:44:29 +0330

    add and Impl SliderFragment.kt

--- app/src/main/java/com/example/imassage_admin/ui/fragment/slider/SliderViewModel.kt
+++ app/src/main/java/com/example/imassage_admin/ui/fragment/slider/SliderViewModel.kt
@@ -1,7 +1,21 @@
 package com.example.imassage_admin.ui.fragment.slider
 
 import androidx.lifecycle.ViewModel
+import com.example.imassage_admin.data.repository.DataRepository
+import okhttp3.MultipartBody
 
-class SliderViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+class SliderViewModel(
+        private val dataRepository: DataRepository
+) : ViewModel() {
+
+    suspend fun sliders() =
+            dataRepository.getSliders()
+
+    suspend fun uploadSlider(image: MultipartBody.Part , title: String , description: String) =
+            dataRepository.uploadSlider(image , title , description)
+
+    suspend fun deleteSlider(id: String) =
+            dataRepository.deleteSlider(id)
+
+
 }
