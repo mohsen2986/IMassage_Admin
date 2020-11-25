c5ae669201f97bc41f3c827a503d18d621b65ac8
mohsen <mohsenaliakbari.atom@hotmail.com>
2020-11-25 03:49:13 +0330

    > Impl ConfigFragment.kt

--- app/src/main/java/com/example/imassage_admin/ui/fragment/config/ConfigFragment.kt
+++ app/src/main/java/com/example/imassage_admin/ui/fragment/config/ConfigFragment.kt
@@ -2,31 +2,55 @@ package com.example.imassage_admin.ui.fragment.config
 
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.imassage_admin.R
+import androidx.navigation.NavController
+import androidx.navigation.fragment.findNavController
+import com.example.imassage_admin.databinding.FragmentConfigBinding
+import com.example.imassage_admin.ui.base.ScopedFragment
+import com.haroldadmin.cnradapter.NetworkResponse
+import kotlinx.coroutines.launch
+import org.kodein.di.Kodein
+import org.kodein.di.KodeinAware
+import org.kodein.di.android.x.closestKodein
+import org.kodein.di.generic.instance
 
-class ConfigFragment : Fragment() {
-
-    companion object {
-        fun newInstance() = ConfigFragment()
-    }
+class ConfigFragment : ScopedFragment(), KodeinAware {
+    override val kodein: Kodein by closestKodein()
+    private val viewModelFactory: ConfigViewModelFactory by instance()
 
     private lateinit var viewModel: ConfigViewModel
+    private lateinit var navController: NavController
+    private lateinit var binding: FragmentConfigBinding
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_config, container, false)
+        binding = FragmentConfigBinding.inflate(inflater , container , false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        navController = findNavController()
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(ConfigViewModel::class.java)
-        // TODO: Use the ViewModel
+        viewModel = ViewModelProvider(this , viewModelFactory).get(ConfigViewModel::class.java)
+        bindUI()
     }
+    private fun bindUI() = launch{
+        when(val callback = viewModel.config()){
+            is NetworkResponse.Success ->{
+                Log.e("Log__" , "${callback.body}")
+            }
+        }
+    }
+
 
 }
--- app/src/main/java/com/example/imassage_admin/ui/fragment/config/ConfigViewModelFactory.kt
+++ app/src/main/java/com/example/imassage_admin/ui/fragment/config/ConfigViewModelFactory.kt
@@ -2,12 +2,14 @@ package com.example.imassage_admin.ui.fragment.config
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import com.example.imassage_admin.data.repository.DataRepository
 
 class ConfigViewModelFactory(
+        private val dataRepository: DataRepository
 ): ViewModelProvider.NewInstanceFactory(){
 
     @Suppress("UNCHECKED_CAST")
     override fun <T : ViewModel?> create(modelClass: Class<T>): T {
-        return ConfigViewModel() as T
+        return ConfigViewModel(dataRepository) as T
     }
 }
